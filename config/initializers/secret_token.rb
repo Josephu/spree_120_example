1ee9025d4c767f9d8a84fea59122dd2fc808e741
jimmywong <imagegospel@gmail.com>
2012-10-26 09:17:58 +0800

    Spree 1.2.0 with advanced report

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spreestore120::Application.config.secret_token = '5f4d296a9e6d7bf78139b7a619ab9fb682831df0993449f4690de3d06465a78a8c0f747f783a7b7a4ef356e2c5809a4e5d107b7be2602580714d24256e825ff3'
